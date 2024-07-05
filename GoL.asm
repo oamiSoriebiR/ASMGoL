36220b8e32ffa7c4554edda98a945fbc6258d402
oamiSoriebiR <simaolvmr@gmail.com>
2024-07-05 23:51:47 +0100

    Create GoL.asm
    First ideas

---
+++ GoL.asm
@@ -0,0 +1,7 @@
+# RISK-V GoL - created by Simão Ribeiro
+# This is a simple implementation of Conway's Game of Life in RISK-V assembly language.
+# I'm using the RV32I instruction set and the RARS simulator.
+# I still wanna decide some things, like the size of the grid, the initial state, etc. 
+# About the features, I want a simple game, just printing the grid in the console and updating it every generation.
+# Regarding the initial state, I'm thinking about a random state, or a user input state.
+
